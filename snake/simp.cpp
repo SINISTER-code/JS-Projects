5ab9fdc210674ddc46c89700deee2d4fe2e49073
SINISTER-code <72219493+SINISTER-code@users.noreply.github.com>
2021-05-25 02:11:40 +0530

    Add files via upload

---
+++ snake/simp.cpp
@@ -0,0 +1,45 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//   int marityu, dard, sambhog;
+//   tuchahiye;
+//   if (!Geol Moment)
+//   {
+//     cout << "That is something i am willing to accept, although 1% >>> 10% [but X2]" << endl;
+//   }
+//   else
+//   {
+//     return simp;
+//   }
+//   return 0;
+// }
+
+
+// #include<bits/stdc++.h>
+// int main(){
+//   int marityu,dard,sambhog;
+//   if (!Geol moment)
+//   {
+//     cout<<"Zinda ladki thapar derabassi roll";
+//     if (!simping)
+//     {
+//       cout<<"102016091"<<endl;
+//     }
+//     else
+//     {
+//       cout<<"Fadla sharmana nhi ata!"<<endl;
+//       for (int i = 0; i > 0 ; i++)
+//       {
+//         cout<<"baad mein baat krte hai"<<endl;
+//       }
+      
+//     }
+//   }
+//   else
+//   {
+//     return Simp;
+//   }
+//   return 0;
+// }
+
